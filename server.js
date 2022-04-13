2b6f1cb8c9d02428b3dd92b7c8f55036af340e6c
RT Flores <me@rtflores.com>
2022-04-13 10:32:09 -0700

    complete all but one test

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 // server.js
 // where your node app starts
 
+// npm install dotenv --save
+// create .env file with environment variables
+require('dotenv').config();
+
 // init project
 var express = require('express');
 var app = express();
@@ -8,6 +12,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const req = require('express/lib/request');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,6 +29,36 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) => {
+  let unixTime = 0;
+  let validDate = false;
+  let dateRegex = /[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])/;
+  if (dateRegex.test(req.params.date)) {
+    unixTime = Date.parse(req.params.date);
+    validDate = true;
+  }
+  if (/[0-9]{13}/.test(req.params.date)) {
+    unixTime = parseInt(req.params.date);
+    validDate = true;
+  }
+  if (!req.params.date) {
+    let currentDate = new Date();
+    res.json({
+      unix: Date.now(),
+      utc: currentDate.toUTCString()
+    })
+  }
+  if (!validDate) {
+    res.json({error: 'Invalid Date'});
+  }
+  
+  let utcTime = new Date(unixTime);
+
+  res.json({
+    unix: unixTime,
+    utc: utcTime.toUTCString()
+  }); 
+});
 
 
 // listen for requests :)
